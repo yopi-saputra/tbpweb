db2815e864d5731423c9aadfff5210b4855fd1aa
nediaputrii <50162029+NediaPutrii@users.noreply.github.com>
2020-04-29 22:23:57 +0700

    cuma coba-coba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
                 <div class="title m-b-md">
                     Laravel
                 </div>
-
+<!-- cuma coba-coba -->
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
