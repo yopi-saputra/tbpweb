ddf1bca17b707222837a1e9b11558e7e29f73534
nediaputrii <50162029+NediaPutrii@users.noreply.github.com>
2020-05-27 14:08:31 +0700

    tambah fitur update
    mengupdate data yang sudah diisi di halaman edit

--- app/Http/Controllers/FrontEnd/Intern/InternGradeController.php
+++ app/Http/Controllers/FrontEnd/Intern/InternGradeController.php
@@ -82,7 +82,30 @@ class InternGradeController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+            //dump($id);
+        //$detailkp=$request->nim;
+        if($request->grade <=100 && $request->grade >=0){
+            DB::table('internships')->where('id',$id)->update([
+                'grade' => $request->grade
+            ]);
+
+            Session::flash('message', 'Nilai berhasil di edit');
+        }
+        else
+        {
+            Session::flash('message', 'Nilai tidak terdefinisi');
+        }
+
+            $detailkp = DB::table('internships')
+            ->select('internships.*', 'internship_agencies.name','students.nim')
+            ->join ('students', 'internships.student_id','=','students.id')
+            ->join ('Lecturers', 'internships.advisor_id','=','lecturers.id')
+            ->join ('internship_proposals', 'internships.internship_proposal_id','=','internship_proposals.id')
+            ->join ('internship_agencies', 'internship_proposals.agency_id','=','internship_agencies.id')
+            ->where ('internships.id', $id)
+            ->get();
+                
+            return view('klp05.show',compact('detailkp'));
     }
 
     /**
